65b102f0e7a00b52c922b2a9675d9ae457557fb0
Matheus <mt.schreiber01@gmail.com>
2023-04-10 11:24:43 -0300

    adding logs

--- server.js
+++ server.js
@@ -1,17 +1,47 @@
 const express = require('express');
-const { startWorker } = require('./index');
-
+const pm2 = require('pm2');
 const app = express();
 
-app.get('/start', (req, res) => {
-  startWorker();
-  
-  setInterval(() => {
-    res.write('\n'); 
-  }, 1000);
+pm2.connect(function(err) {
+  console.log("Starting pm2")
 
+  if (err) {
+    console.error(err);
+    process.exit(2);
+  }
+
+  
+
+
+  pm2.start({
+    script: 'index.js',
+    name: 'worker',
+    log: true,
+    log_level: 'debug'
+  }, function(err, apps) {
+    if (err) {
+      pm2.disconnect();
+      console.error(err);
+      process.exit(2);
+    }
+
+    pm2.launchBus(function(err, bus) {
+      if (err) {
+        console.error(err);
+        process.exit(2);
+      }
+  
+      bus.on('log:out', function(data) {
+        console.log('[App:%s] %s', data.process.name, data.data);
+      });
+  
+      bus.on('log:err', function(data) {
+        console.error('[App:%s][Err] %s', data.process.name, data.data);
+      });
+    });
+  });
 });
 
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+  console.log('Ferias Ufes started on port 3000');
 });
